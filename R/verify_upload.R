e7ee5b646ab36bbb3549efffbf414b37d970c227
Joran Elias <joran.elias@umontana.edu>
2015-12-14 09:41:58 -0700

    move db connection creation into this package, moving towards removing dependency on statskier package

--- R/verify_upload.R
+++ R/verify_upload.R
@@ -1,9 +1,12 @@
 verifyUpload <- function(tbls){
-  con <- statskier::db_xc()
+  con_local <- db_xc_local()
+  con_remote <- db_xc_remote()
   raceid <- tbls$raceid[1]
-  test <- statskier::query(con,"select * from main where raceid = ",raceid)
-  dbDisconnect(con)
-  if (nrow(test) == nrow(tbls)){
+  test_local <- ss_query(con_local,sprintf("select * from main where raceid = %s",raceid))
+  test_remote <- ss_query(con_remote,sprintf("select * from main where raceid = %s",raceid))
+  dbDisconnect(con_local)
+  dbDisconnect(con_remote)
+  if (nrow(test_local) == nrow(tbls) && nrow(test_remote) == nrow(tbls)){
     cat("\nUpload successful!\n")
   }
   else{
