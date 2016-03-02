235c9cd44512cb43faa80bd664c1391595847616
Joran Elias <joran.elias@umontana.edu>
2016-03-02 09:22:58 -0700

    new check_names that tries fisid first, then compid, falls back to doing nothing

--- R/check_names.R
+++ R/check_names.R
@@ -1,17 +1,34 @@
-#' @importFrom plyr ddply
-checkNames <- function(x){
-  #browser()
-  fisids <- paste_in(x$fisid)
+check_names <- function(race){
   con_local <- db_xc_local()
-  res <- ss_query(con_local,sprintf("select 
-                        fisid,
-                        name,
-                        count(distinct raceid) n 
-                       from main 
-                       where fisid in %s
-                       group by fisid,name",fisids))
-  dbDisconnect(con_local)
-  res <- plyr::ddply(res,c("fisid"),function(x){x[which.max(x$n),]})
-  x$name[match(res$fisid,x$fisid)] <- res$name
-  x
+  q_fisid <- "select fisid,name,count(distinct raceid) n 
+              from main where fisid in %s group by fisid,name"
+  q_compid <- "select compid,name,count(distinct raceid) n 
+               from main where compid in %s group by compid,name"
+  if (all(!is.na(race$fisid))){
+    fisids <- paste_in(race$fisid)
+    res <- ss_query(con_local,sprintf(q_fisid,fisids))
+    
+    res <- res %>%
+      group_by(fisid) %>%
+      top_n(1,wt = n)
+    race$name[match(res$fisid,race$fisid)] <- res$name
+    
+    dbDisconnect(con_local)
+    return(race)
+  }else{
+    if (all(!is.na(race$compid))){
+      compids <- paste_in(race$compid)
+      res <- ss_query(con_local,sprintf(q_compid,compids))
+      
+      res <- res %>%
+        group_by(compid) %>%
+        top_n(1,wt = n)
+      race$name[match(res$compid,race$compid)] <- res$name
+      
+      dbDisconnect(con_local)
+      return(race)
+    }else{
+      return(race)
+    }
+  }
 }
