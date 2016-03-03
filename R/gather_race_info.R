7c5dc3627386b539083015a8013a331472818a71
joran <joran.elias@gmail.com>
2016-03-02 18:59:41 -0700

    formatting

--- R/gather_race_info.R
+++ R/gather_race_info.R
@@ -1,46 +1,66 @@
 gatherRaceInfo <- function(){
   while(TRUE){
     cat("\nEnter race info: \n\n")
+    
     dt <- readline(prompt = "Date: ")
+    
     gender <- switch(menu(c('Men','Women')),'Men','Women')
+    
     location <- toupper(readline(prompt = "Location: "))
+    
     cat1 <- toupper(readline(prompt = "Cat1: "))
     cat2 <- toupper(readline(prompt = "Cat2: "))
-    if (cat2 == 'JUNIOR'){cat2 <- 'Junior'}
-    if (cat2 == 'NA'){cat2 <- NA}
+    
+    if (cat2 == 'JUNIOR'){
+      cat2 <- 'Junior'
+    }
+    if (toupper(cat2) == 'NA'){
+      cat2 <- NA
+    }
+    
     type <- switch(menu(c('Distance','Sprint','Stage')),'Distance','Sprint','Stage')
+    
     if (type == 'Distance'){
       start <- switch(menu(c('Interval','Mass','Pursuit','Handicap')),'Interval','Mass','Pursuit','Handicap')
-    }
-    else{
+    }else{
       start <- NA
     }
+    
     if (start == 'Pursuit' & !is.na(start)){
       tech <- 'FC'
-    }
-    else{
+    }else{
       tech <- switch(menu(c('Freestyle','Classic','Classic/Freestyle')),'F','C','FC')
     }
+    
     len <- as.numeric(readline(prompt = "Length: "))
+    
     if (type != 'Sprint'){
       url <- readline(prompt = "URL: ")
       if (grepl(pattern = "^https",x = url)){
         url <- gsub(pattern = "^https",replacement = "http",x = url)
       }
-    }
-    else{
+    }else{
       urlQual <- readline(prompt = "Qualification URL: ")
-      urlFin <- readline(prompt = "Finals URL: ")
+      urlFin <- readline(prompt  = "       Finals URL: ")
+      
       if (grepl(pattern = "^https",x = urlQual)){
-        urlQual <- gsub(pattern = "^https",replacement = "http",x = urlQual)
+        urlQual <- gsub(pattern = "^https",
+                        replacement = "http",
+                        x = urlQual)
       }
+      
       if (grepl(pattern = "^https",x = urlFin)){
-        urlFin <- gsub(pattern = "^https",replacement = "http",x = urlFin)
+        urlFin <- gsub(pattern = "^https",
+                       replacement = "http",
+                       x = urlFin)
       }
+      
       if (toupper(urlQual) == "NA"){urlQual <- NA}
       if (toupper(urlFin) == "NA"){urlFin <- NA}
+      
       url <- list(qual = urlQual,final = urlFin)
     }
+    
     raceInfo <- list(cat1 = cat1,
                      cat2 = cat2,
                      location = location,
@@ -51,6 +71,7 @@ gatherRaceInfo <- function(){
                      date = dt,
                      gender = gender,
                      url = url)
+    
     cat("\n\nIs this correct?\n")
     cat("\nCat1      =",raceInfo$cat1)
     cat("\nCat2      =",raceInfo$cat2)
@@ -61,13 +82,14 @@ gatherRaceInfo <- function(){
     cat("\nStart     =",raceInfo$start)
     cat("\nTechnique =",raceInfo$tech)
     cat("\nLength    =",raceInfo$length)
+    
     if (type != 'Sprint'){
       cat("\nURL       =",raceInfo$url)
-    }
-    else{
+    }else{
       cat("\nQual URL  =",raceInfo$url$qual)
       cat("\nFinal URL =",raceInfo$url$final)
     }
+    
     cat("\n")
     result <- menu(c('Correct','Incorrect'))
     if (result == 1){return(raceInfo)}
