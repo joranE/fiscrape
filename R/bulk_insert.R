e4bcdbaeaf040b34eea4b032bb8629e7330904a5
Joran Elias <joran.elias@umontana.edu>
2016-03-03 13:58:08 -0700

    need to do bulk_insert for sqlite main because of the stupid extra column

--- R/bulk_insert.R
+++ R/bulk_insert.R
@@ -1,7 +1,5 @@
 #' @import RSQLite
-bulk_insert <- function(cn,sql,data)
-{
-  #dbBeginTransation(cn)
+bulk_insert <- function(cn,sql,data){
   dbBegin(cn)
   dbGetPreparedQuery(cn,sql,bind.data=data)
   dbCommit(cn)
