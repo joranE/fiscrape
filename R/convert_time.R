0e2f84e2b2bc6befd632d8842e6e9625c8d74c76
Joran Elias <joran.elias@umontana.edu>
2016-03-09 09:21:38 -0700

    start work on something better for time parsing

--- R/convert_time.R
+++ R/convert_time.R
@@ -30,3 +30,29 @@ convertTime <- function(x,raceType){
   }
   sapply(x,processTime,raceType = raceType)
 }
+
+#' @importFrom stringr str_count
+#' @importFrom lubridate hms
+#' @importFrom lubridate period_to_seconds
+convert_time <- function(times){
+  #Intended to catch "MM:SS.S" or "HH:MM:SS.S" formats
+  pat <- "^([0-9]{1,2}:){1,2}([0-9]{2}\\.[0-9]{1})$"
+  format_ok <- grepl(pattern = pat,x = times)
+  
+  if (!all(format_ok)){
+    #Show what the times look like and have user specify format...?
+    n_bad <- sum(!format_ok)
+    cat(n_bad,"out of",length(times),"race times have an unusual format:\n")
+    print(times[!format_ok])
+    stop("Bad times need to be fixed.")
+  }else{
+    #Convert as usual
+    one_colon <- stringr::str_count(times,":") == 1
+    if (any(one_colon)){
+      times[one_colon] <- paste0("00:",times[one_colon])
+    }
+    
+    secs <- lubridate::period_to_seconds(lubridate::hms(times))
+  }
+  secs
+}
