096313d21f08df544c6729ec7d70366f9eb3f9c7
Joran Elias <joran.elias@umontana.edu>
2016-03-24 13:41:32 -0600

    re-oxygenize

--- R/verify_upload.R
+++ R/verify_upload.R
@@ -1,9 +1,9 @@
 verify_upload <- function(tbls){
-  con_local <- db_xc_local()
-  con_remote <- db_xc_remote()
+  con_local <- statskier2::db_xc_local()
+  con_remote <- statskier2::db_xc_remote()
   raceid <- tbls$raceid[1]
-  test_local <- ss_query(con_local,sprintf("select * from main where raceid = %s",raceid))
-  test_remote <- ss_query(con_remote,sprintf("select * from main where raceid = %s",raceid))
+  test_local <- statskier2::ss_query(con_local,sprintf("select * from main where raceid = %s",raceid))
+  test_remote <- statskier2::ss_query(con_remote,sprintf("select * from main where raceid = %s",raceid))
   dbDisconnect(con_local)
   dbDisconnect(con_remote)
   if (nrow(test_local) == nrow(tbls) && nrow(test_remote) == nrow(tbls)){
