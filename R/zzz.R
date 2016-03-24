d5369ce302827fd365688e88eadb03cb9fc4dfbf
Joran Elias <joran.elias@umontana.edu>
2016-03-24 13:42:15 -0600

    use statskier2 to put data source in options()

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,14 @@
 `%ni%` <- Negate(`%in%`)
+
+.onAttach <- function(libname,pkgname){
+  src <- statskier2:::choose_src()
+  options(statskier_src = src)
+  invisible()
+}
+
+.onDetach <- function(libpath){
+  if (!is.null(options()$statskier_src) && inherits(options()$statskier_src,"src_mysql")){
+    DBI::dbDisconnect(options()$statskier_src$con)
+  }
+  invisible()
+}
