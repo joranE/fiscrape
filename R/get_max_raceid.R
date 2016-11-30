def5d020404e8fed62964fe07e6caeb4dc3062f9
joran <joran.elias@gmail.com>
2016-11-29 18:37:01 -0700

    better naming

--- R/get_max_raceid.R
+++ R/get_max_raceid.R
@@ -1,6 +1,12 @@
-getMaxRaceID <- function(){
+get_max_raceid <- function(){
   con_local <- statskier2::db_xc_local()
-  res <- statskier2::ss_query(con_local,"select max(raceid) mx from main")
+  con_remote <- statskier2::db_xc_remote()
+  res_local <- statskier2::ss_query(con_local,"select max(raceid) mx from main")
+  res_remote <- statskier2::ss_query(con_remote,"select max(raceid) mx from main")
+  if (res_local$mx != res_remote$mx){
+    stop("Max raceid in local/remote out of sync.")
+  }
   dbDisconnect(con_local)
-  res$mx
+  dbDisconnect(con_remote)
+  res_local$mx
 }
