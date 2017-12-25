4b4d180ed3ff7a9b4849ba933f088cc3d4f668d2
joran <joran.elias@gmail.com>
2017-12-25 16:41:07 -0700

    better db update syntax

--- R/delete_race.R
+++ R/delete_race.R
@@ -69,15 +69,19 @@ update_race <- function(raceid,...){
 #' Update race info in both remote and local
 #' databases for a specified raceid.
 #'
-#' @param fisid integer
+#' @param target_gender character
+#' @param target_compid integer
+#' @param target_fisid character
 #' @param ... named changes, e.g. \code{length = 30} or \code{cat1 = "WC"}
 #' @export
-update_person <- function(target_fisid,...){
+update_person <- function(target_gender,target_compid,target_fisid,...){
   conr <- statskier2::db_xc_remote()
   conl <- statskier2::db_xc_local()
   person <- statskier2::ss_query(conl,
-                     sprintf("select distinct fisid,name,gender,yob,nation from main where fisid = '%s'",
-                             target_fisid))
+                     sprintf("select distinct fisid,name,gender,yob,nation 
+                              from main 
+                              where fisid = '%s' and gender = '%s' and compid = %s",
+                             target_fisid,target_gender,target_compid))
   cat("Preparing to update the following person:\n")
   print(person)
   changes <- list(...)
@@ -93,9 +97,45 @@ update_person <- function(target_fisid,...){
     cat("\nExiting and doing nothing...")
     return(NULL)
   }else{
-    statskier2::ss_query(conr,sprintf("update main set %s where fisid = '%s'",set_clause,target_fisid))
-    statskier2::ss_query(conl,sprintf("update main set %s where fisid = '%s'",set_clause,target_fisid))
+    statskier2::ss_query(conr,
+                         sprintf("update main set %s 
+                                  where fisid = '%s' and gender = '%s' and compid = %s",
+                                 set_clause,target_fisid,target_gender,target_compid))
+    statskier2::ss_query(conl,
+                         sprintf("update main set %s 
+                                  where fisid = '%s' and gender = '%s' and compid = %s",
+                                 set_clause,target_fisid,target_gender,target_compid))
   }
   dbDisconnect(conl)
   dbDisconnect(conr)
+}
+
+#' @export
+fix_ath_names <- function(){
+  conr <- statskier2::db_xc_remote()
+  conl <- statskier2::db_xc_local()
+  
+  aths <- ss_query(con = conl,
+                   q = "select gender,compid,fisid,name,max(date) max_date,
+                              count(distinct raceid) n_race from main
+                        where fisid is not null and compid is not null
+                        group by gender,compid,fisid,name")
+  
+  ath_dups <- aths %>%
+    group_by(gender,compid,fisid) %>%
+    mutate(n_id = n_distinct(name)) %>%
+    filter(n_id > 1) %>%
+    ungroup() %>%
+    arrange(gender,compid,fisid,desc(n_race),desc(max_date)) %>%
+    mutate(split_var = paste(gender,compid,fisid,sep = "-"))
+  
+  ath_dups <- split(ath_dups,ath_dups$split_var)
+  
+  for (i in seq_along(ath_dups)){
+    cur <- ath_dups[[i]]
+    update_person(target_gender = cur$gender[1],
+                  target_compid = cur$compid[1],
+                  target_fisid = cur$fisid[1],
+                  name = cur$name[1])
+  }
 }
