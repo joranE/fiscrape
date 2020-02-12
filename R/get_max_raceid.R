f7a91ae2fd2825333da62bf33aac921338626c98
Joran Elias <joran.elias@umontana.edu>
2020-02-12 16:36:27 -0700

    update function to fetch max raceid

--- R/get_max_raceid.R
+++ R/get_max_raceid.R
@@ -1,12 +1,10 @@
+#' @export
 get_max_raceid <- function(){
-  con_local <- statskier2::db_xc_local()
-  con_remote <- statskier2::db_xc_remote()
-  res_local <- statskier2::ss_query(con_local,"select max(raceid) mx from main")
-  res_remote <- statskier2::ss_query(con_remote,"select max(raceid) mx from main")
-  if (res_local$mx != res_remote$mx){
-    stop("Max raceid in local/remote out of sync.")
-  }
-  dbDisconnect(con_local)
-  dbDisconnect(con_remote)
-  res_local$mx
+  res_local <- RSQLite::dbGetQuery(conn = conl,
+                                   statement = "select 'dst' as event_type,max(raceid) mx_raceid from dst_event 
+                                                union  
+                                                select 'spr' as event_type,max(raceid) mx_raceid from spr_event
+                                                union
+                                                select 'stg' as event_type,max(raceid) mx_raceid from stg_event")
+  max(res_local$mx_raceid)
 }
