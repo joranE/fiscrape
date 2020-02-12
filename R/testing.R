18c2f6b2208eb7c9fdcd47ac9160585e5ceb23da
joran <joran.elias@gmail.com>
2020-02-11 20:03:37 -0700

    what the heck is wrong?

--- R/testing.R
+++ R/testing.R
@@ -1,5 +1,7 @@
+library(dplyr)
 library(RSQLite)
-conl <- RSQLite::dbConnect(RSQLite::SQLite(),dbname = "~/Dropbox/new-results-db/output/fis_results_prototype.db")
+conl <- RSQLite::dbConnect(RSQLite::SQLite(),
+                           dbname = "~/Dropbox/new-results-db/output/fis_results_prototype.db")
 src_skier <- tbl(conl,"skier")
 #### Distance Races ####
 #Interval start
@@ -15,7 +17,7 @@ race_info <- list(date = "2020-02-01",
                   cat2 = NA)
 url <- "https://www.fis-ski.com/DB/general/results.html?sectorcode=CC&raceid=35122"
 
-#x <- fiscrape:::dst_scrape(url = url,raceInfo = race_info)
+x <- fiscrape:::dst_scrape(url = url,race_info = race_info)
 
 #Mass start
 race_info <- list(date = "2020-01-18",
@@ -81,3 +83,70 @@ race_info <- list(date = "2020-02-07",
                   cat1 = "FIS",
                   cat2 = NA)
 url <- "https://www.fis-ski.com/DB/general/results.html?sectorcode=CC&raceid=36112"
+
+#Prototyping the process for screening skier records that don't match anything
+# currently in the db
+new_skiers <- anti_join(x[[2]],src_skier,by = c("compid","fisid","name","yob"),copy = TRUE)
+true_new <- list()
+for (i in seq_len(nrow(new_skiers))){
+  row <- new_skiers[i,]
+  cap_name <- stringr::str_extract(string = new_skiers$name[i],"^[-A-Z]+")
+  cap_name <- paste0("%",cap_name,"%")
+  poss_match <- src_skier %>% 
+    filter(compid %in% local(new_skiers$compid[i]) | 
+             fisid %in% local(new_skiers$fisid[i]) | 
+             name %like% cap_name) %>% 
+    collect()
+  skier_comp <- bind_rows(list(existing = poss_match,new = new_skiers[i,]),.id = "ref") %>%
+    select(-birth_date)
+  diff_vals <- names(skier_comp)[sapply(skier_comp,function(x) n_distinct(x) > 1)][-1]
+  n <- nrow(skier_comp)
+  
+  print(skier_comp)
+  
+  if (n == 2){
+    s <- select.list(choices = diff_vals,
+                     multiple = TRUE,
+                     graphics = FALSE,
+                     title = "Which values (if any) should we update?")
+    if (length(s) == 0) next
+    ref_compid <- skier_comp$compid[1]
+    updates <- as.list(skier_comp[2,s])
+    print(update_skier(ref_compid = ref_compid,updates = updates))
+    cat("\n")
+    Sys.sleep(0.5)
+  }
+  
+  if (n > 2){
+    m <- menu(choices = seq_len(n - 1),
+              graphics = FALSE,
+              title = "Which existing record is a match? (0 for no match)")
+    
+    #No match; potentially new skier
+    if (m == 0){
+      true_new <- c(true_new,row)
+    }
+    skier_comp <- skier_comp[c(m,n),]
+    diff_vals <- names(skier_comp)[sapply(skier_comp,function(x) n_distinct(x) > 1)][-1]
+    print(skier_comp)
+    s <- select.list(choices = diff_vals,
+                     multiple = TRUE,
+                     graphics = FALSE,
+                     title = "Which values (if any) should we update?")
+    if (length(s) == 0) next
+    ref_compid <- skier_comp$compid[1]
+    updates <- as.list(skier_comp[2,s])
+    print(update_skier(ref_compid = ref_compid,updates = updates))
+    cat("\n")
+    Sys.sleep(1)
+  }
+  true_new <- bind_rows(true_new)
+  if (nrow(true_new) == 0){
+    cat("\nNo new skiers to insert.")
+  } else {
+    print(true_new)
+    cat("\nInserting these new skiers.")
+  }
+}
+
+l <- list(compid = 1,fisid = "1234567",name = "Foo Bar",yob = 1987)
--- R/trim_compids.R
+++ R/trim_compids.R
@@ -1,3 +1,4 @@
+#' @export
 trim_compids <- function(x){
   if (length(x) < 3) return(x)
   if (all(x[1:3] == x[4:6])){
